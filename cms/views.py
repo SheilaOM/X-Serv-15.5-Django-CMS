12e5e894c4734f4f4f5800f1e78ca2a149425d50
SheilaOM <sheila.oliva14@gmail.com>
2017-03-27 20:20:21 +0200

    Revisado tras check

--- cms/views.py
+++ cms/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from cms.models import Pages
-
 # Create your views here.
+
+
 def show(request):
     resp = "Las direcciones disponibles son: "
     lista_pages = Pages.objects.all()
     for page in lista_pages:
         resp += "<br>-/" + page.name + " --> " + page.page
     return HttpResponse(resp)
+
 
 def process(request, req):
     try:
